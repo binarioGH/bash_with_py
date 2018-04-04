e888d41e7334e2a0f14861f4304efec4252a2c41
binarioGH <lacuentafalsadediegojonas@gmail.com>
2018-04-03 22:13:34 -0500

    se cambia la estructura del programa

--- bash_with_py.py
+++ bash_with_py.py
@@ -1,6 +1,34 @@
 #-*- coding: utf-8 -*-
 import subprocess
 import os
+
+def clear():
+	subprocess.call(["cmd.exe","/c","cls"])
+
+def ls():
+	dirlist = os.listdir('.')
+	for file in dirlist:
+		print(file)
+
+def mkdir():
+	try:
+	    os.mkdir(cmd[6:])
+	except Exception as e: 
+		print("ese directorio ya existe") 
+
+def cd(cdir):
+	
+	try:
+		os.chdir(cdir)
+	except Exception as e:
+		print("el sistema no puede encontrar la ruta")
+def rm(delfd):
+	try:
+		os.remove(cmd[2:])
+	except Exception as e:
+		print("no se ha encontrado la carpeta o directorio {}".format(delfd))
+
+
 if __name__ == '__main__':
 	wt = True
 	while wt:
@@ -9,31 +37,29 @@ if __name__ == '__main__':
 			print('''de momento no pondré este comando porque el output es largo y hace dificil 
 leer el codigo''')
 		elif cmd == "clear":
-			subprocess.call(["cmd.exe","/c","cls"])
+			clear()
+
 		elif cmd [:2] == "ls" and cmd[2:] == "":
-			dirlist = os.listdir('.')
-			for file in dirlist:
-				print(file)
+			ls()
+
 		elif cmd[:6] == "mkdir ":
-			try:
-				os.mkdir(cmd[6:])
-			except Exception as e: 
-				print("ese directorio ya existe") 
-		elif cmd[:3] == "cd ":
-			try:
-				os.chdir(cmd[3:])
-			except Exception as e:
-				print("el sistema no puede encontrar la ruta")
+			mkdir()
+
+		elif cmd[:2] == "cd":
+			cd(cmd[3:])
+
 		elif cmd[:2] == "rm":
-			try:
-				os.remove(cmd[3:])
-			except Exception as e:
-				print("carpeta o archivo '{}' no encontrado".format(cmd[3:]))
+			rm(cmd[2:])
+
+
 		elif cmd[:4] == "echo":
 			print(cmd[5:])
 		elif cmd[:5] == "touch":
-			open(cmd[6:], "w")
+			open(cmd[5:], "w")
 		elif cmd[:3] == "pwd":
-			print("{}".format(os.getcwd()))
+			print('''
+		{}
+				'''.format(os.getcwd()))
+
         
         
