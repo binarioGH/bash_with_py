8589907dfdecb215c90c411849764127ddd1702c
binarioGH <lacuentafalsadediegojonas@gmail.com>
2018-04-03 20:57:14 -0500

    se agrega el comando 'pwd'

--- bash_with_py.py
+++ bash_with_py.py
@@ -1,22 +1,19 @@
 #-*- coding: utf-8 -*-
 import subprocess
 import os
-# de momento este programa no esta tan avanzado, pero con el tiempo lo voy a mejorar.
 if __name__ == '__main__':
 	wt = True
 	while wt:
 		cmd = str(raw_input("{}>".format(os.getcwd())))
 		if cmd == "help":
 			print('''de momento no pondré este comando porque el output es largo y hace dificil 
-leer el codigo''') 	
+leer el codigo''')
 		elif cmd == "clear":
 			subprocess.call(["cmd.exe","/c","cls"])
 		elif cmd [:2] == "ls" and cmd[2:] == "":
 			dirlist = os.listdir('.')
-			
 			for file in dirlist:
 				print(file)
-						
 		elif cmd[:6] == "mkdir ":
 			try:
 				os.mkdir(cmd[6:])
@@ -36,9 +33,7 @@ leer el codigo''')
 			print(cmd[5:])
 		elif cmd[:5] == "touch":
 			open(cmd[6:], "w")
-			
-
-
-
-
-		    		
+		elif cmd[:3] == "pwd":
+			print("{}".format(os.getcwd()))
+        
+        
